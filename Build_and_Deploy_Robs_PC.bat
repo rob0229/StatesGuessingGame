f38dcd41de8733c98b9aa9135169d933770fa569
rob0229 <rob0229@gmail.com>
2015-11-01 17:49:14 -0500

    Corrected bat file

--- Build_and_Deploy_Robs_PC.bat
+++ Build_and_Deploy_Robs_PC.bat
@@ -4,6 +4,6 @@ cd C:\Developer\\apache-tomcat-8.0.23-windows-x64\\apache-tomcat-8.0.23\\bin
 call shutdown.bat
 cd C:\\Developer\\apache-tomcat-8.0.23-windows-x64\\apache-tomcat-8.0.23\\webapps
 rm -rf StatesGuessingGame*
-cp C:\\Developer\\StatesGuessingGame\\target\\StatesGuessingGame.war C:\\Developer\\apache-tomcat-8.0.23-windows-x64\\apache-tomcat-8.0.23\\webapps
+copy C:\Developer\StatesGuessingGame\target\StatesGuessingGame.war C:\Developer\apache-tomcat-8.0.23-windows-x64\apache-tomcat-8.0.23\webapps
 cd C:\\Developer\\apache-tomcat-8.0.23-windows-x64\\apache-tomcat-8.0.23\\bin
 call startup.bat
