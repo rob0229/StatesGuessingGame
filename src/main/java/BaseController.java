197240c8231101a57a9ef83b3a6f6ebb93977f9e
rob0229 <rob0229@gmail.com>
2015-11-01 21:06:58 -0500

    All components are functioning correctly. The basic architecture and frameworks are fully operational. Ready to add pages and begin development of game
    Signed-off-by: rob0229 <rob0229@gmail.com>

--- src/main/java/BaseController.java
+++
@@ -1,7 +0,0 @@
-
-public class BaseController {
-	
-	public BaseController(){
-		System.out.println("I am working");
-	}
-}
--- src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h1>With Controller</h1>
-<h2>Hello World, Rob was here</h2>
-</body>
-</html>
